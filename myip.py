62aa84dd03fdc88ff58dc94e1b809aeaf5e9e704
see2 <wse532@126.com>
2019-07-12 11:47:04 +0800

    simple ip test file

---
+++ myip.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+class MyIp:
+  def  __init__(self):
+    self.ip=self.get()
+
+  def get(self):  
+    # use this website to get the ip
+    url = "https://www.myip.com/"
+    response =  requests.get(url)
+    soup = BeautifulSoup(response.text)
+    b = soup.find("span", id="ip")
+    return b.text
+  
+  def __repr__(self):
+      return "<Your IP is :%s>" % (self.ip)
+
+  def __str__(self):
+      return "From str method of Test: Your IP is :%s" % (self.ip)
